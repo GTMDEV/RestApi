fb07cc48e3b6205f0425e0fe861ae11dc0249a75
GTMDEV <dev.goutamv@gmail.com>
2023-07-22 23:22:25 +0530

    Created Post entity

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,13 +1,16 @@
 package com.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -25,6 +28,10 @@ public class User {
 	@Past(message = "Birth Date should be in the past")
 	@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
 
 	public User(Integer id, String userName, LocalDate birthDate) {
 		super();
@@ -60,8 +67,8 @@ public class User {
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
-  
-	@Override
+
+	@Override 
 	public String toString() {
 		return "User [id=" + id + ", userName=" + userName + ", birthDate=" + birthDate + "]";
 	}
--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -6,3 +6,13 @@ values(10002,current_date(),'Ravi');
 
 insert into user_details(id,birth_date,user_name)
 values(10003,current_date(),'Pradeep');
+
+insert into post(id,description,user_id)
+values(20001,'I want learn AWS',10001);
+
+insert into post(id,description,user_id)
+values(20002,'I want learn Devops',10002);
+
+insert into post(id,description,user_id)
+values(20003,'I want to get AWS certified',10003);
+
