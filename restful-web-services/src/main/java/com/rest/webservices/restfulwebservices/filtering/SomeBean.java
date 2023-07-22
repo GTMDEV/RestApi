b6f9500cb41c450f6d67b4bdf5675507a85917b8
GTMDEV <dev.goutamv@gmail.com>
2023-07-22 17:15:30 +0530

    Ignore field at class level, JsonIgnoreProperties

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,12 +1,14 @@
 package com.rest.webservices.restfulwebservices.filtering;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties({"field1","field2"})
 public class SomeBean {
 
 	private String field1;
 	
-	@JsonIgnore
+	//@JsonIgnore
 	private String field2;
 	
 	private String field3;
