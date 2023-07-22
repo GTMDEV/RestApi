d402234ca0e1f40c6d5b03a04addbbdd4d60093a
GTMDEV <dev.goutamv@gmail.com>
2023-07-22 16:50:33 +0530

    Added Hateoas

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,9 +1,12 @@
 package com.rest.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,13 +36,16 @@ public class UserResource {
 
 	@GetMapping("/users/{id}")
 
-	public User retrieveUser(@PathVariable int id) {
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
 		if (user == null) {
 			throw new UserNotFoundException("id: " + id);
 
 		}
-		return user;
+		EntityModel<User> entityModel = EntityModel.of(user);
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-user"));
+		return entityModel;
 	}
 
 	@DeleteMapping("/users/{id}")
