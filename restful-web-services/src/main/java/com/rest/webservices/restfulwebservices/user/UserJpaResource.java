53feda978f56c718c3ed6c0568d63dda955dc3d6
GTMDEV <dev.goutamv@gmail.com>
2023-07-23 00:26:32 +0530

    save api for post

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserJpaResource.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserJpaResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rest.webservices.restfulwebservices.jpa.PostRepository;
 import com.rest.webservices.restfulwebservices.jpa.UserRepository;
 
 import jakarta.validation.Valid;
@@ -28,11 +29,14 @@ public class UserJpaResource {
 
 	private UserRepository repository;
 
+	private PostRepository postRepository;
+
 	@Autowired
-	public UserJpaResource(UserDaoService service, UserRepository repository) {
+	public UserJpaResource(PostRepository postRepository, UserRepository repository) {
 		super();
 
 		this.repository = repository;
+		this.postRepository = postRepository;
 	}
 
 	@GetMapping("/jpa/users")
@@ -77,6 +81,20 @@ public class UserJpaResource {
 		User savedUser = repository.save(user);
 
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+	@PostMapping("/jpa/users/{id}/posts")
+
+	public ResponseEntity<Object> createPostForUser(@PathVariable int id, @Valid @RequestBody Post post) {
+		Optional<User> user = repository.findById(id);
+		if (user.isEmpty())
+			throw new UserNotFoundException("id :" + id);
+
+		post.setUser(user.get());
+		Post savedPost = postRepository.save(post);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedPost.getId())
 				.toUri();
 		return ResponseEntity.created(location).build();
 	}
