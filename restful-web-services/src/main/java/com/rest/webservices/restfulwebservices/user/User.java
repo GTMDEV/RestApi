bae52c67cc26f84bb5bd45a7c70ae382dd1d947f
GTMDEV <dev.goutamv@gmail.com>
2023-07-22 19:41:18 +0530

    Connection with H2 Database

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -4,22 +4,32 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
+	@Id
+	@GeneratedValue
 	private Integer id;
-	@Size(min = 2,message="Name should have atleast 2 Characters")
+
+	@Column(name = "user_name")
+	@Size(min = 2, message = "Name should have at least 2 Characters")
 	@JsonProperty("user_name")
-	private String name;
+	private String userName;
+
 	@Past(message = "Birth Date should be in the past")
 	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 
-	public User(Integer id, String name, LocalDate birthDate) {
+	public User(Integer id, String userName, LocalDate birthDate) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.userName = userName;
 		this.birthDate = birthDate;
 	}
 
@@ -32,11 +42,11 @@ public class User {
 	}
 
 	public String getName() {
-		return name;
+		return userName;
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		this.userName = name;
 	}
 
 	public LocalDate getBirthDate() {
@@ -49,7 +59,7 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+		return "User [id=" + id + ", name=" + userName + ", birthDate=" + birthDate + "]";
 	}
 
 }
