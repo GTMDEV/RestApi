f23b164e3f2b096dbe50bd722a9a910daa2df21d
GTMDEV <dev.goutamv@gmail.com>
2023-07-22 18:13:04 +0530

    Optimize Dynamic filtering

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -17,22 +17,23 @@ public class FilteringController {
 	@GetMapping("/filtering")
 	public MappingJacksonValue filtering() {
 		SomeBean someBean = new SomeBean("value1", "value2", "value3");
-		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
-		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
-		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
-		mappingJacksonValue.setFilters(filters);
-		return mappingJacksonValue;
+		return applyFilter(someBean, "field1", "field3");
 	}
 
 	@GetMapping("/filtering-list")
 	public MappingJacksonValue filteringList() {
 		List<SomeBean> list = Arrays.asList(new SomeBean("value1", "value2", "value3"),
 				new SomeBean("value4", "value5", "value6"));
-		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(list);
+		return applyFilter(list, "field2", "field3");
+	}
 
-		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3");
+	private MappingJacksonValue applyFilter(Object object, String... fields) {
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(object);
+
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(fields);
 		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
 		mappingJacksonValue.setFilters(filters);
+
 		return mappingJacksonValue;
 	}
 }
